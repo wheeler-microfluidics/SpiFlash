99f199a7944571a0b483842d7aadd171e4788e2c
Christian Fobel <christian@fobel.net>
2017-01-31 16:17:20 -0500

    [NB] Use templated SoftSpi for SPI comm

--- SpiFlash.h
+++ SpiFlash.h
@@ -2,8 +2,10 @@
 #define ___SPI_FLASH__H___
 
 
+#include <stdint.h>
 #include <Arduino.h>
-#include <SPI.h>
+//#include <SPI.h>
+#include <SoftSpi.h>
 #include <CArrayDefs.h>
 
 /*
@@ -66,13 +68,13 @@
  * [1]: https://cdn.sparkfun.com/datasheets/Dev/Teensy/w25q64fv.pdf
  */
 
-class SpiFlash {
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode = 0>
+class SpiFlashBase {
 protected:
   uint8_t ERROR_CODE_;
-  SPISettings spi_settings_;
+  typedef SoftSPI<MisoPin, MosiPin, SckPin, Mode> SoftSPI_t;
+  SoftSPI_t soft_spi_;
 
-  bool disable_write();
-  bool enable_write();
   void deselect_chip();
   void select_chip();
   void set_error(uint8_t error_code) { ERROR_CODE_ = error_code; }
@@ -104,10 +106,13 @@ public:
 
   static const uint8_t TIMEOUT_ERROR = 0x10;
 
-  SpiFlash() : ERROR_CODE_(0), cs_pin_(0), device_id_(0),
-               manufacturer_id_(0) {}
-  SpiFlash(uint8_t cs_pin) : ERROR_CODE_(0), cs_pin_(cs_pin), device_id_(0),
-                             manufacturer_id_(0) {}
+  bool disable_write();
+  bool enable_write();
+
+  SpiFlashBase() : ERROR_CODE_(0), cs_pin_(0), device_id_(0),
+                   manufacturer_id_(0) {}
+  SpiFlashBase(uint8_t cs_pin) : ERROR_CODE_(0), cs_pin_(cs_pin),
+                                 device_id_(0), manufacturer_id_(0) {}
 
   void begin();
   void begin(uint8_t cs_pin);
@@ -115,7 +120,6 @@ public:
   uint8_t error_code() const { return ERROR_CODE_; }
   void clear_error() { set_error(0); }
 
-  void set_spi_settings(SPISettings const &spi_settings);
   uint8_t status_register1();
   uint8_t status_register2();
 
@@ -130,5 +134,241 @@ public:
   bool write_page(uint32_t address, UInt8Array src);
 };
 
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+void SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::deselect_chip() {
+  digitalWrite(cs_pin_, HIGH);
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+void SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::select_chip() {
+  digitalWrite(cs_pin_, LOW);
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+void SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::begin() {
+  pinMode(cs_pin_, OUTPUT);
+  soft_spi_.begin();
+
+  select_chip();
+  soft_spi_.transfer(INSTR__MANUFACTURER_DEVICE_ID);
+  for (int i = 0; i < 3; i++) { soft_spi_.transfer(0); }
+  manufacturer_id_ = soft_spi_.transfer(0);
+  device_id_ = soft_spi_.transfer(0);
+  deselect_chip();
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+void SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::begin(uint8_t cs_pin) {
+  cs_pin_ = cs_pin;
+  begin();
+}
+
+/*
+ * # Read status register 1 #
+ *
+ *  1. Select chip
+ *  2. Send `Read Status Register-1`:
+ *      * Shift out: `[0x05]`
+ *      * Shift out `[0xXX]`, shift in `status`
+ *  3. Deselect chip
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+uint8_t SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::status_register1() {
+  select_chip();
+  soft_spi_.transfer(INSTR__READ_STATUS_REGISTER_1);
+  uint8_t status = soft_spi_.transfer(0);
+  deselect_chip();
+  return status;
+}
+
+/*
+ * # Read status register 2 #
+ *
+ *  1. Select chip
+ *  2. Send `Read Status Register-2`:
+ *      * Shift out: `[0x35]`
+ *      * Shift out `[0xXX]`, shift in `status`
+ *  3. Deselect chip
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+uint8_t SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::status_register2() {
+  select_chip();
+  soft_spi_.transfer(INSTR__READ_STATUS_REGISTER_2);
+  uint8_t status = soft_spi_.transfer(0);
+  deselect_chip();
+  return status;
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::ready() {
+  return !(status_register1() & STATUS__BUSY);
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>
+::ready_wait(uint32_t timeout) {
+  uint32_t start = millis();
+
+  while (!ready()) {
+    if ((millis() - start) > timeout) {
+      set_error(TIMEOUT_ERROR);
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * # Read #
+ *
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>
+::read(uint32_t address, uint8_t *dst, uint32_t length) {
+  //  1. Check that device is ready (see "Wait for ready")
+  if (!ready_wait()) { return false; }
+
+  //  2. Select chip
+  select_chip();
+  /*  3. Send `Read Data`
+   *      * Shift out: `[0x03]` */
+  soft_spi_.transfer(INSTR__READ_DATA);
+  //      * Shift out: `[A23-A16][A15-A8][A7-A0]`
+  soft_spi_.transfer(address >> (2 * 8));  // A23-A16
+  soft_spi_.transfer(address >> (1 * 8));  // A15-A8
+  soft_spi_.transfer(address);  // A7-A0
+  //  4. Shift out `[0xXX]`, shift in value
+  //  5. Repeat 4 to read bytes as needed.
+  for (uint32_t i = 0; i < length; i++) {
+    dst[i] = soft_spi_.transfer(0);
+  }
+  //  6. Deselect chip
+  deselect_chip();
+  clear_error();
+  return true;
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+UInt8Array SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>
+::read(uint32_t address, UInt8Array dst) {
+  if (!read(address, dst.data, dst.length)) {
+    dst.data = NULL;
+    dst.length = 0;
+  }
+  return dst;
+}
+
+// Read single byte from address.
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+uint8_t SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::read(uint32_t address) {
+  uint8_t value = 0;
+  read(address, &value, 1);
+  return value;
+}
+
+/*
+ * # Write enable #
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::enable_write() {
+  select_chip();
+  soft_spi_.transfer(INSTR__WRITE_ENABLE);
+  deselect_chip();
+  // Verify expected state of write enable bit in status register.
+  return status_register1() & STATUS__WRITE_ENABLE;
+}
+
+/*
+ * # Write disable #
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::disable_write() {
+  select_chip();
+  soft_spi_.transfer(INSTR__WRITE_DISABLE);
+  deselect_chip();
+  // Verify expected state of write enable bit in status register.
+  return !(status_register1() & STATUS__WRITE_ENABLE);
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>::erase_chip() {
+  /* 1. Check that:
+   *      - Device is ready (see "Wait for ready")
+   *      - Write is enabled (see "Write enable") */
+  if (!ready_wait() || !enable_write()) { return false; }
+
+  select_chip();
+  //  2. Send `Chip erase`
+  soft_spi_.transfer(INSTR__CHIP_ERASE);
+  deselect_chip();
+  /*  3. Wait for up to 60s for erase to complete.
+   *
+   * Notes:
+   *
+   *  - `BUSY` bit in status register remains set until erase is complete.
+   *  - Write enable bit in status register is cleared upon erase completion.
+   */
+  if (!ready_wait(60000L /* 60 seconds */)) {
+    disable_write();
+    return false;
+  }
+  return true;
+}
+
+/*
+ * # Write page (i.e., up to 256 bytes) #
+ */
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>
+::write_page(uint32_t address, uint8_t *src, uint32_t length) {
+  /*  1. Check that:
+   *      * Device is ready (see "Wait for ready")
+   *      * Write is enabled (see "Write enable") */
+  if (!ready_wait() || !enable_write()) { return false; }
+
+  //  2. Select chip
+  select_chip();
+  /*  3. Send `Page Program`
+   *      * Shift out: `[0x02]`
+   *      * Shift out: `[A23-A16][A15-A8][A7-A0]`
+   */
+  soft_spi_.transfer(INSTR__PAGE_PROGRAM);
+  soft_spi_.transfer(address >> (2 * 8));  // A23-A16
+  soft_spi_.transfer(address >> (1 * 8));  // A15-A8
+  soft_spi_.transfer(address);  // A7-A0
+  /*  4. Shift out `N` bytes
+   *      * **NOTE** bytes will be written to:
+   *
+   *             [A23-A16][A15-A8][0x00] + ((i + [A7-A0]) % 256)
+   *
+   *      * In other words, addresses wrap modulo 256.
+   *      * To write 256 contiguous bytes starting at specified address,
+   *      address must be 256-byte aligned (i.e., `[A7-A0]` must be 0).
+   */
+  for (uint32_t i = 0; i < length; i++) {
+    soft_spi_.transfer(src[i]);
+  }
+  //  5. Deselect chip
+  deselect_chip();
+  /*  6. Wait for up to 100ms for write to complete.
+   *
+   * Notes:
+   *
+   *  - `BUSY` bit in status register remains set until write is complete.
+   *  - Write enable bit in status register is cleared upon write completion.
+   */
+  if (!ready_wait(100)) {
+    disable_write();
+    return false;
+  }
+  return true;
+}
+
+template<uint8_t MisoPin, uint8_t MosiPin, uint8_t SckPin, uint8_t Mode>
+bool SpiFlashBase<MisoPin, MosiPin, SckPin, Mode>
+::write_page(uint32_t address, UInt8Array src) {
+  return write_page(address, src.data, src.length);
+}
 
 #endif  // #ifndef ___SPI_FLASH__H___
